13551ec3709bdee53fb43f30063cb219c52ad446
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-05-04 18:47:25 +0300

    update

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const hangmanGame = document.querySelector('.hangman-article')
 const overlayImg = hangmanGame.querySelector('.overlay-image')
+const answer = hangmanGame.querySelector('.answer-text')
+
+const words = ['nikita']
+const word = words[Math.floor(Math.random() * words.length)].split('');
+
+const reserv = wordsArray => {
+  let reservedSpace = "" 
+  for (let i = 0; i < wordsArray.length; i += 1) {
+    reservedSpace += "_"
+  }
+  return answer.textContent = reservedSpace
+}
+
+reserv(word)
+
+
 
 window.addEventListener('keydown', onWrongKey)
+window.addEventListener('keydown', onGoodKey)
 
 let defaultPosition = -165
 
 function onWrongKey(event) {
   if (event.code === 'KeyS') {
     if (defaultPosition < 75) {
-      console.log('sosi');
-
       defaultPosition -= -20
     
       overlayImg.style.top = `${defaultPosition}px`
@@ -17,6 +32,16 @@ function onWrongKey(event) {
     } else {
       defaultPosition = -165
       overlayImg.style.top = `${defaultPosition}px`
+    }
+  }
+}
+
+
+function onGoodKey(event) {
+  const latterKey = word.map((item) => `Key${item.toUpperCase()}`)
+  if (latterKey.includes(event.code)) {
+    for (let i = 0; i < word.length; i+=1){
+      answer.textContent.split('')[i] = word[i]
     }
   }
 }
